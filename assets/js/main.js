515a02abafd596c9c05180ff7a3c2a55e42fd3c4
Giorgio <giorgio.pignattari@gmail.it>
2021-03-17 15:48:11 +0100

    creato input html e inizio js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Consegna
+// Creare una Todo-list in cui si possano aggiungere nuovi todo e rimuoverli.
+// Rendiamo il tutto gradevole alla vista.
+// Bonus
+// Permettiamo la modifica di un todo già aggiunto.
+
+$(document).ready(function(){
+
+});

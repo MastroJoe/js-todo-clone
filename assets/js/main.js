646a4470179ad5cc6aea1f666223d5325d4654f2
Giorgio <giorgio.pignattari@gmail.it>
2021-03-17 17:07:39 +0100

    To-Do list funzionante

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,5 +5,18 @@
 // Permettiamo la modifica di un todo già aggiunto.
 
 $(document).ready(function(){
-
+  // al click salvo il valore inserito dall'utente
+  $('#add').click(function(){
+    var toDo = $('#toDoIn').val();
+    $('#toDoIn').val('');
+    // clono li e sostituisco il valore con il valore inserito dall'utente
+    var template = $('#toDo li').clone();
+    template.children('span.userToDo').text(toDo);
+    // al click di delete la To-Do viene rimossa
+    template.children('span.delete').click(function() {
+      $(this).parent().remove();
+    });
+    // stampo il valore su HTML 
+    $('#todos').append(template);
+  })
 });
